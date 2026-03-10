b73e7fff4498c0f2d7c04f257dba1d44e0ffdda8
BrunsAraujo <bruno.correadearaujo@gmail.com>
2026-03-09 22:16:05 -0500

    Updated/created Turnherefullstackapplication.java

---
+++ Backend/turnherefullstack2/src/main/java/com/turnhere/TurnherefullstackApplication.java
@@ -0,0 +1,13 @@
+package com.turnhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnherefullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnherefullstackApplication.class, args);
+    }
+
+}
