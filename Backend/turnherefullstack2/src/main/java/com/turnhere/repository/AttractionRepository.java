1d56d854bcf767a3f5d6cd2d3ec44606468c4af4
BrunsAraujo <bruno.correadearaujo@gmail.com>
2026-03-09 21:37:51 -0500

    Changed package name, and altered configurations on the tree

---
+++ Backend/turnherefullstack2/src/main/java/com/turnhere/repository/AttractionRepository.java
@@ -0,0 +1,15 @@
+package com.turnhere.repository;
+
+
+
+import com.turnhere.models.Attraction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AttractionRepository extends JpaRepository<Attraction, Long> {
+
+    List<Attraction> findByCityId(Long cityId);
+
+    List<Attraction> findByType(Attraction.Type type);
+}
